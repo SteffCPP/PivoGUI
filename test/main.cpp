962ec361a2ffdd2d0bd509e46db85839486e6ff0
Rando Stefano <randostefano39@proton.me>
2026-04-22 11:07:05 +0200

    making game in main.cpp file

--- test/main.cpp
+++ test/main.cpp
@@ -2,16 +2,36 @@
 #include "PIVO/PIVO.hpp"
 
 int main(){
-	pivo::Window win("Finestra", {1080, 980});
+	pivo::Window win("Finestra", {1080, 980}, {0, 200, 255});
 
-	pivo::Circle circle(50.0f, {500 ,500}, pivo::colors::Green);
-	win.assign(circle);
+	const float rectSizeX=50;
+	const float rectSizeY=50;
+	float rectPosX = 300;
+	float rectPosY = 300;
 
-	pivo::Rectangle rect{{300, 300}, {60, 60}, pivo::colors::Red};
+	pivo::Rectangle rect{{rectSizeX, rectSizeY}, {rectPosX, rectPosY}};
 	win.assign(rect);
+	
+	pivo::Rectangle ground{{1080, 300}, {0, 980-300}, pivo::colors::Green};
+	win.assign(ground);
 
+	int accel=0;
+	bool canJump=true;
 	while(win.isOpen()){
 		pivo::Input_Manager::update();
+		
+		if(++accel>=40) accel=40;
+		if(pivo::Keyboard::isPressed(pivo::Key::SPACE) && canJump){
+			accel = -20;
+			canJump = false;
+		}
+		rect.move({0, (float)accel});
+
+		if(rect.getPosition().y+rectSizeY >= ground.getPosition().y){
+			rect.setPosition({rectPosX, ground.getPosition().y-rectSizeY});
+			canJump = true;
+		}
+
 		win.update();
 	}
 
